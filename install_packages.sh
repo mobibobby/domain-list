f376d92734983cbfe0851844dfb4b1c37b359507
Vadim Koustov <vkoustov@husky.ca>
2024-08-04 20:34:51 -0400

    short list

--- install_packages.sh
+++ install_packages.sh
@@ -1,5 +1,38 @@
 #!/bin/sh
 
+#DNSMasq Full
+#opkg update
+#opkg update && cd /tmp/ && opkg download dnsmasq-full
+#opkg remove dnsmasq && opkg install dnsmasq-full --cache /tmp/
+
+VNPs
+#opkg install wireguard-tools
+#opkg install openconnect
+#opkg install luci-proto-openconnect
+#opkg install sing-box
+#opkg install dnscrypt-proxy2
+
+Wireguard Server
+#opkg update
+#opkg install luci-proto-wireguard qrencode
+ 
+Tools
+#opkg install curl
+#opkg install bind-dig
+
+USB
+#opkg install kmod-usb-storage kmod-usb2 kmod-fs-vfat block-mount
+
+DDNS
+#opkg update
+#opkg install ddns-scripts
+#opkg install ddns-scripts-noip
+#opkg install luci-app-ddns 
+#opkg install wget ca-certificates
+#opkg install curl ca-bundle
+#ddns-scripts-noip  wget-ssl  ca-certificates curl ca-bundle 
+
+
 # List of packages to install
 PACKAGES="nano curl wget luci"
 
